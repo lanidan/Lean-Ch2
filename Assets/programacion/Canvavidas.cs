ccdc2fb7eddc47893aee715e93e9255cacca6f69
lanidan <chiarelloleandro6@gmail.com>
2023-10-19 17:47:46 -0300

    agregar sistema de vidas

---
+++ Assets/programacion/Canvavidas.cs
@@ -0,0 +1,23 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Canvavidas : MonoBehaviour
+{
+
+    static public int vida; //Variable que contiene las vidas. Dice static al inicio para que otros scripts puedan acceder a esta variable
+    public Animator animvida; //Variable que toma el Animator
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animvida = GetComponent<Animator>(); //tomamos el animator y lo guardamos en la variable animvida
+    }
+       void Update()
+    {
+        animvida.SetInteger("vida", vida); //Vinculamos el script con el parametro de la animacion
+    }
+}
