54e2b92ab8957f340828784a6f4e25e1c14f2067
lanidan <chiarelloleandro6@gmail.com>
2023-11-02 20:49:17 -0300

    agregamos particulas

--- Assets/programacion/EnemigoMovil.cs
+++ Assets/programacion/EnemigoMovil.cs
@@ -40,4 +40,17 @@ public class EnemigoMovil : MonoBehaviour{
       
         
     }
+  void OnTriggerEnter2D(Collider2D col){  //llamamos a la funcion cuando trigger entra al collider 2D
+        if (col.gameObject.tag == "Player"){ //Se lee como "Si la colision se produce con un tag jugador"
+            
+           float yResta = 0.7f; //Esto lo usamos como ajuste. Si no está este ajuste, cuando el jugador lo toca de frente se eliminaria
+           if(transform.position.y + yResta < col.transform.position.y){ // Se lee como "Si la posicion del enemigo es menor a la posicion del jugador entonces..."
+                col.SendMessage("SaltarEnemigo"); //Hacemos un llamado a saltar Enemigo a Controles cuando lo pisamos 
+                Destroy(gameObject); //Esta linea es la encargada de desruir el objeto
+           } else { //Sino
+                   Canvavidas.vida -=2;
+               col.SendMessage("RetrocesoEnemigo", transform.position.x);// Enviamos un mensaje a Controles con Retroceso enemigo y enviamos como argumento la posicion en x del enemigo
+           }
+        }
+    }
 }
